8f7e0454fe8ad73ab1e97b90b504b9a37d57d52d
g82411 <lmz8241@nisra.net>
2014-05-03 23:10:38 +0800

    login.php完成

--- controller/login.php
+++ controller/login.php
@@ -1,15 +1,22 @@
 <?php
+session_start();
 require 'connect.php';
 $username = mysql_real_escape_string($_POST['username']);
-$passwd = mysql_real_escape_string($_POST['passwd']);
-$result=mysql_query("SELECT Cookie,uid FROM user WHERE id =".$username." password =".$passwd.";" );
+$passwd = mysql_real_escape_string($_POST['password']);
+$result=mysql_query("SELECT Cookie,uid FROM user WHERE id =".$username." password =".$password.";" );
 if(mysql_fetch_array($result)){
-	setcookie("permittion",$result['Cookie']);
+	/*setcookie("permittion",$result['Cookie']);
 	setcookie("uid",$result['uid']);
-	echo "True";
+	echo "True";*/
+	$jsonRseponse = array("Cookie" => $result['Cookie'],
+		"confirm" => True);
+	echo json_encode($jsonRseponse);
+	$_SESSION["Cookie"] = $result['Cookie'];
+
 }
 else{
-	echo "False";
+	$jsonRseponse = array("confirm" => False);
+	echo json_encode($jsonRseponse);
 }
 mysql_close();
 ?>
