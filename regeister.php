aae9fa9d3a89045cfb5a2f1229576c25e5af64a7
asd3582 <asd3582asd3582@yahoo.com.tw>
2014-05-03 11:39:32 +0900

    5/3 11:40

---
+++ regeister.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+<link href="css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+
+<div style="width:500px; height:400px; background-color:red;position:relative;top:100px; left:80px; "></div>
+<input id=".ID" type="id" class="form-control" id="exampleInputid" placeholder="ID" style="width:300px; height:50px;position:relative;top:-290px; left:600px; ">
+<input id=".password" type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" style="width:300px; height:50px;position:relative;top:-290px; left:600px; ">
+<input id=".recheckpassword" type="recheckpassword" class="form-control" id="exampleInputrecheckpassword1" placeholder="Recheck Password" style="width:300px; height:50px;position:relative;top:-290px; left:600px; ">
+<input id=".nickname" type="nickname" class="form-control" id="exampleInputnickname1" placeholder="nickname" style="width:300px; height:50px;position:relative;top:-290px; left:600px; ">
+<input id=".Country" type="Country" class="form-control" id="exampleInputCountry1" placeholder="Country" style="width:300px; height:50px;position:relative;top:-290px; left:600px; ">
+<input id=".email" type="Profession" class="form-control" id="exampleInputemail1" placeholder="email" style="width:300px; height:50px;position:relative;top:-290px; left:600px; ">
+<input id=".Profession" type="Profession" class="form-control" id="exampleInputProfession1" placeholder="Profession" style="width:300px; height:50px;position:relative;top:-290px; left:600px; ">
+
+<button id=".register"  type="button" class="btn btn-default" style="width:100px; height:50px;position:relative;top:-230px; left:700px; " >Register</button>
+
+
+
+
+
+
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/Index.js"></script>
+
+</body>
+</html>
