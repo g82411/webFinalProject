c18c28a7778b4ab2058407f5a8bedb9bcb64b29a
asd3582 <asd3582asd3582@yahoo.com.tw>
2014-05-07 15:07:35 +0800

    5/7
    Ajax未完成

--- Index.php
+++ Index.php
@@ -1,15 +1,42 @@
 <html>
 <head>
 <link href="css/bootstrap.min.css" rel="stylesheet">
+<head>
+<script type="text/javascript"> </script>
+    <script>
+         var Login=function(){
+            var URLs="\controller\check_login.php";
+           
+            $.ajax({
+                url: URLs,
+                data: $('#.ID').serialize(),
+                type:"POST",
+                dataType:'text',
+
+                success: function(msg){
+                    alert(msg);
+                },
+
+                 error:function(xhr, ajaxOptions, thrownError){ 
+                    alert(xhr.status); 
+                    alert(thrownError); 
+                 }
+            });
+            
+        }
+       
+
+</script>
 </head>
 
 <body>
 
 <div style="width:500px; height:400px; background-color:red;position:relative;top:100px; left:80px; "></div>
-<input id=".ID" type="id" class="form-control" id="exampleInputID" placeholder="ID" style="width:300px; height:50px;position:relative;top:-290px; left:600px; ">
-<input id=".password" type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" style="width:300px; height:50px;position:relative;top:-200px; left:600px; ">
+<input id=".ID" type="id" class="form-control" id="ID" placeholder="ID" style="width:300px; height:50px;position:relative;top:-290px; left:600px; ">
+<input id=".password" type="password" class="form-control" id="Password" placeholder="Password" style="width:300px; height:50px;position:relative;top:-200px; left:600px; ">
 
-<button id=".login" type="button" class="btn btn-default" style="width:100px; height:50px;position:relative;top:-100px; left:600px; " >Login</button>
+<button id=".login" type="button" class="btn btn-default" style="width:100px; height:50px;position:relative;top:-100px; left:600px; " onclick="login()">Login</button>
+
 <button id=".register"  type="button" class="btn btn-default" style="width:100px; height:50px;position:relative;top:-100px; left:700px; " >Register</button>
 
 
--- regeister.php
+++ regeister.php
@@ -8,7 +8,7 @@
 <div style="width:500px; height:400px; background-color:red;position:relative;top:100px; left:80px; "></div>
 <input id=".ID" type="id" class="form-control" id="exampleInputid" placeholder="ID" style="width:300px; height:50px;position:relative;top:-290px; left:600px; ">
 <input id=".password" type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" style="width:300px; height:50px;position:relative;top:-290px; left:600px; ">
-<input id=".recheckpassword" type="recheckpassword" class="form-control" id="exampleInputrecheckpassword1" placeholder="Recheck Password" style="width:300px; height:50px;position:relative;top:-290px; left:600px; ">
+<input id=".recheckpassword" type="password" class="form-control" id="exampleInputrecheckpassword1" placeholder="Recheck Password" style="width:300px; height:50px;position:relative;top:-290px; left:600px; ">
 <input id=".nickname" type="nickname" class="form-control" id="exampleInputnickname1" placeholder="nickname" style="width:300px; height:50px;position:relative;top:-290px; left:600px; ">
 <input id=".Country" type="Country" class="form-control" id="exampleInputCountry1" placeholder="Country" style="width:300px; height:50px;position:relative;top:-290px; left:600px; ">
 <input id=".email" type="Profession" class="form-control" id="exampleInputemail1" placeholder="email" style="width:300px; height:50px;position:relative;top:-290px; left:600px; ">
