475d45d2603bc498f9061e0a66f1ca1015a0dabc
asd3582 <asd3582asd3582@yahoo.com.tw>
2014-05-19 15:38:08 +0800

    5/19 登入後頁面(index2 )書頁

---
+++ 書頁.php
@@ -0,0 +1,125 @@
+<html>
+<head>
+<link href="css/bootstrap.min.css" rel="stylesheet">
+
+<head>
+<script type="text/javascript"> </script>
+   
+ <style type="text/css"> 
+    table {border-style:double;} 
+	td {border-style:double;width:300} 
+	tr {border-style:double;width:300} 
+	
+	</style> 
+</head>
+
+<body>
+<nav class="navbar navbar-default" role="navigation">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="Index.php">首頁</a> <!--NavBar 首頁按鈕 -->
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Link</a></li>
+        <li><a href="#">Link</a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Action</a></li>
+            <li><a href="#">Another action</a></li>
+            <li><a href="#">Something else here</a></li>
+            <li class="divider"></li>
+            <li><a href="#">Separated link</a></li>
+            <li class="divider"></li>
+            <li><a href="#">One more separated link</a></li>
+          </ul>
+        </li>
+      </ul>
+      <form class="navbar-form navbar-left" role="search">
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="Search">
+        </div>
+        <button type="submit" class="btn btn-default">Submit</button>
+      </form>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#">Link</a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Action</a></li>
+            <li><a href="#">Another action</a></li>
+            <li><a href="#">Something else here</a></li>
+            <li class="divider"></li>
+            <li><a href="#">Separated link</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+<table style="width:60% ;height:320px;position:relative;top:3%; left:20%; "><tr><td>
+<div style="width:200px; height:300px; background-color:red;position:relative;top:10%; left:10%; ">BK</div>
+<table style="position:absolute;top:3%; left:30%; ">
+<tr>
+<td><p>書名</p></td>
+<td><p>1</p></td>
+</tr>
+<tr>
+<td><p>作者</p></td>
+<td><p>2</p></td>
+</tr>
+<tr>
+<td><p>ISBN</p></td>
+<td><p>3</p></td>
+</tr>
+<tr>
+<td><p>日期</p></td>
+<td><p>1</p></td>
+</tr>
+<tr>
+<td><p>commit數</p></td>
+<td><p>2</p></td>
+</tr>
+<tr>
+<td><p>提供者</p></td>
+<td><p>3</p></td>
+</tr>
+<tr>
+<td><p>resource</p></td>
+<td><p>1</p></td>
+</tr>
+<tr>
+<td><p>type</p></td>
+<td><p>2</p></td>
+</tr>
+<tr>
+<td><p>Language</p></td>
+<td><p>3</p></td>
+</tr>
+</table>
+
+</td></tr></table>
+<table style="width:60% ;height:320px;position:relative;top:5%; left:20%; ">
+<tr>
+<td>Commit</td>
+<td>Data</td>
+<td>User</td>
+</tr>
+</table>
+
+ 
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/Index.js"></script>
+</body>
+</html>
