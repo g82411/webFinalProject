e3623e33664a20fbe86d99a139374564f303f4b6
james897897 <taiwan88888@gmail.com>
2014-05-22 20:54:43 +0800

    dump_commet

---
+++ controller/dump_commet.php
@@ -0,0 +1,22 @@
+
+<?php
+require 'connect.php';
+
+while($commitResult=mysql_fetch_array(mysql_query("SELECT CID,BID,UID,title,content,date FROM commit;"))){
+$username=mysql_fetch_array(mysql_query("SELECT username FROM User where UID=".$commitResult['UID'].";"));
+echo  "<div class=\"accordion-group\">";
+echo  "<div class=\"accordion-heading\">"; 
+echo      "<a class=\"accordion-toggle\" data-toggle=\"collapse\" data-parent=\"#accordion2\" href=\"#collapse".$commitResult['UID']."\">";
+echo      $commitResult['title']."   ".$username."    ".$commitResult['dare'];
+echo      "</a>";
+echo    "</div>";
+echo    "<div id=\"collapse".$commitResult['UID']."\" class=\"accordion-body collapse in\">";
+echo      "<div class=\"accordion-inner\">";
+echo        $commitResult['content'];
+echo      "</div>";
+echo    "</div>";
+echo  "</div>";
+}
+ 
+mysql_close();
+?>
--- controller/register.php
+++ controller/register.php
@@ -6,15 +6,15 @@ $passwd = mysql_real_escape_string($_POST['password']);
 $nickname = mysql_real_escape_string($_POST['nickname']);
 $language = mysql_real_escape_string($_POST['language']);
 $profession = mysql_real_escape_string($_POST['profession']);
-$result=mysql_query("SELECT Cookie,uid FROM user WHERE id =".$username." password =".$password.";" );
+$result=mysql_query("SELECT Cookie,uid FROM User WHERE id =".$username." password =".$password.";" );
 if(!$username||!$passwd||!$nickname||!$language||!$profession){
 	echo 'NullValue';
 }
-if(!mysql_fetch_array(mysql_query("SELECT ID,nickname FROM user where ID=".$username." or nickname=".$nickname.";"))){
+if(!mysql_fetch_array(mysql_query("SELECT ID,nickname FROM User where ID=".$username." or nickname=".$nickname.";"))){
 	echo 'userExsist';
 }
 else{	
-	mysql_query("INSERT INTO user(ID,password,nickname,Language,profession)
+	mysql_query("INSERT INTO User(ID,password,nickname,Language,profession)
 	VALUES(".$username.",".$passwd.",".$nickname.",".$language.",".$profession.");");
 	echo 'input_sucess';
 }
