d20204d90fb336b1484be2d18c96191daf976806
james897897 <taiwan88888@gmail.com>
2014-05-22 19:22:43 +0800

    CSS

--- regeister.php
+++ regeister.php
@@ -8,26 +8,30 @@
 	<body>
 
 		<article id="introduction"></article>
-<div class="input-group" id="emailInput">
-  <span class="input-group-addon">Email</span>
-  <input type="text" class="form-control" id="email" placeholder="email">
-</div>
+
 		<input id="id" type="text" class="form-control" placeholder="ID" >
 		<input id="password" type="password" class="form-control" placeholder="Password" >
 		<input id="recheckpassword" type="password" class="form-control" placeholder="Recheck Password" >
 		<input id="nickname" type="text" class="form-control" placeholder="nickname" >
+		
+		<div class="input-group" id="emailInput">
+			
+			<span id="Email" class="input-group-addon" >Email</span>
+			<input type="text" class="form-control" id="email" placeholder="email">
+		</div>
 		<select id="language">
 			<option value="default" selected="selected">擅長語言</option>
 			<option value="Japanese">日文</option>
 			<option value="English">英文</option>
 			<option value="French">法文</option>
+			<!--<option value="Else">其他</option>-->
 		</select>
-		<input id="email" type="email" class="form-control" placeholder="email" >
 		<select id="profession">
 			<option value="default" selected="selected">職業</option>
 			<option value="CSIE">資訊工程</option>
 			<option value="Art">藝術</option>
 			<option value="Ecnomic">經濟</option>
+			<!--<option value="Else">其他</option>-->
 		</select>
 		<button id="register"  type="button" class="btn btn-default" >Register</button>
 
