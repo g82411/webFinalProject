eab6357e434ca6420c14e2d88e8fc24e65465aca
james897897 <taiwan88888@gmail.com>
2014-05-22 17:37:18 +0800

    CSS調整

--- regeister.php
+++ regeister.php
@@ -1,36 +1,39 @@
+<!DOCTYPE html>
 <html>
-<head>
-<link href="css/bootstrap.min.css" rel="stylesheet">
-<link rel="stylesheet" type="text/css" href="./css/regeister.css">
-</head>
+	<head>
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+		<link rel="stylesheet" type="text/css" href="./css/regeister.css">
+	</head>
 
-<body>
+	<body>
 
-<article id="introduction"></article>
+		<article id="introduction"></article>
+<div class="input-group" id="emailInput">
+  <span class="input-group-addon">Email</span>
+  <input type="text" class="form-control" id="email" placeholder="email">
+</div>
+		<input id="id" type="text" class="form-control" placeholder="ID" >
+		<input id="password" type="password" class="form-control" placeholder="Password" >
+		<input id="recheckpassword" type="password" class="form-control" placeholder="Recheck Password" >
+		<input id="nickname" type="text" class="form-control" placeholder="nickname" >
+		<select id="language">
+			<option value="default" selected="selected">擅長語言</option>
+			<option value="Japanese">日文</option>
+			<option value="English">英文</option>
+			<option value="French">法文</option>
+		</select>
+		<input id="email" type="email" class="form-control" placeholder="email" >
+		<select id="profession">
+			<option value="default" selected="selected">職業</option>
+			<option value="CSIE">資訊工程</option>
+			<option value="Art">藝術</option>
+			<option value="Ecnomic">經濟</option>
+		</select>
+		<button id="register"  type="button" class="btn btn-default" >Register</button>
 
-<input id="ID" type="id" class="form-control" id="exampleInputid" placeholder="ID" style="width:300px; height:50px;position:relative;top:-290px; left:600px; ">
-<input id="password" type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" style="width:300px; height:50px;position:relative;top:-290px; left:600px; ">
-<input id="recheckpassword" type="password" class="form-control" id="exampleInputrecheckpassword1" placeholder="Recheck Password" style="width:300px; height:50px;position:relative;top:-290px; left:600px; ">
-<input id="nickname" type="nickname" class="form-control" id="exampleInputnickname1" placeholder="nickname" style="width:300px; height:50px;position:relative;top:-290px; left:600px; ">
-<select id="language">
-    <option value="default" selected="selected">擅長語言</option>
-    <option value="japan">日文</option>
-    <option value="english">英文</option>
-    <option value="french">法文</option>
-</select>
-<input id="email" type="email" class="form-control" id="exampleInputemail1" placeholder="email" style="width:300px; height:50px;position:relative;top:-290px; left:600px; ">
-<select id="pro">
-    <option value="default" selected="selected">職業</option>
-    <option value="CSIE">資訊工程</option>
-    <option value="Art">藝術</option>
-    <option value="ecnomic">經濟</option>
-</select>
-<button id="register"  type="button" class="btn btn-default" style="width:100px; height:50px;position:relative;top:-230px; left:700px; " >Register</button>
+	</body>
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/regeister.js"></script>
 
-</body>
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-<script src="js/bootstrap.min.js"></script>
-<script src="js/regeister.js"></script>
-
-</body>
 </html>
