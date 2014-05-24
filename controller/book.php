242009b557093bc5c77caa2d585a58b4d85dd91a
james897897 <taiwan88888@gmail.com>
2014-05-24 17:00:43 +0800

    book_fix

--- controller/book.php
+++ controller/book.php
@@ -1,9 +1,13 @@
 <?php
 require 'connect.php';
 while($bookDump=mysql_fetch_array(mysql_query("SELECT * FROM Book ORDER BY date LIMIT 0,20;"))){
-	echo $bookDump['booktitle'];
+	echo $bookDump['bookTitle'];
 	echo $bookDump['author'];
 	echo $bookDump['ISBN'];
-	echo
+	echo $bookDump['date'];
+	echo $bookDump['hot'];
+	echo $bookDump['resource'];
+	echo $bookDump['type'];
+	echo $bookDump['Language'];
 }
 ?>
