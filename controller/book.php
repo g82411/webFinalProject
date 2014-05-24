02ecff3c210486326563a51c4d2c7fc8968c9bdb
james897897 <taiwan88888@gmail.com>
2014-05-24 17:24:47 +0800

    book_fix

--- controller/book.php
+++ controller/book.php
@@ -1,6 +1,6 @@
 <?php
 require 'connect.php';
-while($bookDump=mysql_fetch_array(mysql_query("SELECT * FROM Book ORDER BY date LIMIT 0,20;"))){
+while($bookDump=mysql_fetch_array(mysql_query("SELECT * FROM Book;"))){
 	echo $bookDump['bookTitle'];
 	echo $bookDump['author'];
 	echo $bookDump['ISBN'];
