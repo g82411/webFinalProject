1cb409aa162c594ffa223064eca66d1b3011a4f0
lagane100 <lagane100@lagane100deMacBook-Air.local>
2014-05-24 20:25:17 +0800

    book dump

--- controller/book.php
+++ controller/book.php
@@ -1,14 +1,15 @@
 <?php
 require 'connect.php';
 $resultA=mysql_query("SELECT * FROM Book;");
-while($bookDump=mysql_fetch_array($resultA)){
-	echo $bookDump['bookTitle'];
-	echo $bookDump['author'];
-	echo $bookDump['ISBN'];
-	echo $bookDump['date'];
-	echo $bookDump['hot'];
-	echo $bookDump['resource'];
-	echo $bookDump['type'];
-	echo $bookDump['Language'];
-}
+	while($bookDump=mysql_fetch_array($resultA)){
+		echo "<tr><td><p>書名</p></td><td><p>".$bookDump['bookTsitle']."</p></td></tr>";
+    	echo "<tr><td><p>作者</p></td><td><p>".$bookDump['author']."</p></td></tr>";
+    	echo "<tr><td><p>ISBN</p></td><td><p>".$bookDump['ISBN']."</p></td></tr>";
+    	echo "<tr><td><p>日期</p></td><td><p>".$bookDump['date']."</p></td></tr>";
+    	echo "<tr><td><p>commit數</p></td><td><p>".$bookDump['hot']."</p></td></tr>";
+    	echo "<tr><td><p>提供者</p></td><td><p>".$bookDump['author']."</p></td></tr>";
+    	echo "<tr><td><p>resource</p></td><td><p>".$bookDump['resource']."</p></td></tr>";
+    	echo "<tr><td><p>type</p></td><td><p>".$bookDump['type']."</p></td></tr>";
+    	echo "<tr><td><p>Language</p></td><td><p>".$bookDump['Language']."</p></td></tr>";
+	}
 ?>
