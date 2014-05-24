7c337ae2df99af332895ad18c13c1034c305df4e
lagane100 <lagane100@lagane100deMacBook-Air.local>
2014-05-24 21:41:07 +0800

    book test

--- controller/book.php
+++ controller/book.php
@@ -8,7 +8,7 @@ $resultA=mysql_query("SELECT * FROM Book;");
     	echo "<tr><td><p>日期</p></td><td><p>".$bookDump['date']."</p></td></tr>";
     	echo "<tr><td><p>commit數</p></td><td><p>".$bookDump['hot']."</p></td></tr>";
     	echo "<tr><td><p>提供者</p></td><td><p>".$bookDump['author']."</p></td></tr>";
-    	echo "<tr><td><p>resource</p></td><td>""<a href=\"".$bookDump['resource']."\">".$bookDump['resource']."</a></td></tr>";
+    	echo "<tr><td><p>resource</p></td><td><a href=\"" . $bookDump['resource'] . "\">" . $bookDump['resource'] . "</a></td></tr>";
     	echo "<tr><td><p>type</p></td><td><p>".$bookDump['type']."</p></td></tr>";
     	echo "<tr><td><p>Language</p></td><td><p>".$bookDump['Language']."</p></td></tr>";
 	}
