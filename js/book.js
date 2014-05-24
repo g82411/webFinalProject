675bf11d4c21b74540cc7e4f9c1461abcac25aee
lagane100 <lagane100@lagane100deMacBook-Air.local>
2014-05-24 18:26:40 +0800

    repudiate book.js

--- js/book.js
+++ js/book.js
@@ -3,15 +3,8 @@ $(function(){
     type: "GET",
     url: "./controller/book.php",
     dataType:'text';
-    success: function(msg){
-      alert(msg);
-    },
-    error:function(xhr, ajaxOptions, thrownError){ 
-      alert(xhr.status); 
-      alert(thrownError); 
-    }
-    .done(function(test) {
+  })
+  .done(function(test) {
       $('#test').html(test);
-    })
   })
 })
