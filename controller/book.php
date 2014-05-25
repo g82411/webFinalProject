e3189cb91b7d598682884de5ba0d7c459bc43552
g82411 <lmz8241@nisra.net>
2014-05-26 00:17:45 +0800

    JSONtest

--- controller/book.php
+++ controller/book.php
@@ -2,7 +2,8 @@
 require 'connect.php';
 $resultA=mysql_query("SELECT * FROM Book;");
 	while($bookDump=mysql_fetch_array($resultA)){
-		echo "<tr><td><p>書名</p></td><td><p>".$bookDump['bookTitle']."</p></td></tr>";
+		echo json_encode($bookDump);
+        /*echo "<tr><td><p>書名</p></td><td><p>".$bookDump['bookTitle']."</p></td></tr>";
     	echo "<tr><td><p>作者</p></td><td><p>".$bookDump['author']."</p></td></tr>";
     	echo "<tr><td><p>ISBN</p></td><td><p>".$bookDump['ISBN']."</p></td></tr>";
     	echo "<tr><td><p>日期</p></td><td><p>".$bookDump['date']."</p></td></tr>";
@@ -10,6 +11,6 @@ $resultA=mysql_query("SELECT * FROM Book;");
     	echo "<tr><td><p>提供者</p></td><td><p>".$bookDump['author']."</p></td></tr>";
     	echo "<tr><td><p>resource</p></td><td><a href=\"" . $bookDump['resource'] . "\">" . $bookDump['resource'] . "</a></td></tr>";
     	echo "<tr><td><p>type</p></td><td><p>".$bookDump['type']."</p></td></tr>";
-    	echo "<tr><td><p>Language</p></td><td><p>".$bookDump['Language']."</p></td></tr>";
+    	echo "<tr><td><p>Language</p></td><td><p>".$bookDump['Language']."</p></td></tr>";*/
 	}
 ?>
--- controller/search.php
+++ controller/search.php
@@ -1,14 +1,9 @@
 <?php
 	require 'connect.php';
 	$search_string = mysql_real_escape_string($_POST['search_string']);
-	$search_string = htmlentities($search_string , ENT_NOQUOTES,"UTF-8");
-	//mysql_query($search_string);
-	//$result = mysql_query($con, "SELECT * FROM USER");
-	//while($row = mysqli_fetch_array($result)) {
 		$search_result = mysql_query((SELECT * FROM 'USER' WHERE 'TABLES' LIKE '%$search_string%'));
 		while($row = mysqli_fetch_array($search_result)){
 			echo $row['BID'] . " " . $row['bookTitle']; 
 		}
-	//}
 	mysql_close();
 ?>
