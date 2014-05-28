dcbf77992d279608b526609c3bb84ac61a9c73d7
james897897 <taiwan88888@gmail.com>
2014-05-28 20:05:18 +0800

    bookDump

---
+++ controller/hotDump.php
@@ -0,0 +1,9 @@
+<?php
+require 'connect.php';
+$hot=mysql_query("SELECT BID,bookTitle,imageURL,hot FROM Book ORDER BY hot DESC LIMIT 20");
+	while($hotDump=mysql_fecharray($hot)){
+	echo json_encode($hotDump);
+	}
+
+mysql_close();
+?>
