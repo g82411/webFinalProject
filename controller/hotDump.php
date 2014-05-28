fcff97f739ab8400dcf2a7772aacbb5ac9b02600
james897897 <taiwan88888@gmail.com>
2014-05-28 20:10:41 +0800

    fix

--- controller/hotDump.php
+++ controller/hotDump.php
@@ -1,7 +1,7 @@
 <?php
 require 'connect.php';
 $hot=mysql_query("SELECT BID,bookTitle,imageURL,hot FROM Book ORDER BY hot DESC LIMIT 20");
-	while($hotDump=mysql_fecharray($hot)){
+	while($hotDump=mysql_fetch_array($hot)){
 	echo json_encode($hotDump);
 	}
 
