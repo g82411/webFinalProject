dae6d4dff6965199c53369a70ca7d1307163ce92
g82411 <lmz8241@nisra.net>
2014-06-02 00:28:09 +0800

    newBookTest

--- controller/newBook.php
+++ controller/newBook.php
@@ -5,6 +5,7 @@ $ISBN=htmlentities(mysql_escape_string($_POST['ISBN']));
 $language=htmlentities(mysql_escape_string($_POST['language']));
 $profession=htmlentities(mysql_escape_string($_POST['profession']));
 $email=htmlentities(mysql_escape_string($_POST['email']));
-mysql_query("INSERT INTO Book (bookTitle,author,imageURL,ISBN,date,hot,resource,type,Language) VALUES 
-	('".$bookTitle."','".$author."','".$imageUrl."','".$ISBN."','".date("Y-m-d")."','0','".$resource."',,'".$language."')");
+mysql_query("INSERT INTO Book (bookTitle,author,imageURL,ISBN,date,hot,resource,profession,Language) VALUES 
+	('".$bookTitle."','".$author."','".$imageUrl."','".$ISBN."','".date("Y-m-d")."','0','".$resource."','".$profession."','".$language."');");
+
 ?>
