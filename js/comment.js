b796ba91b3d6d30d840917f469d72e3332aa8fb9
lagane100 <lagane100@lagane100deMacBook-Air.local>
2014-06-02 17:11:16 +0800

    add tinymce

---
+++ js/comment.js
@@ -0,0 +1,9 @@
+tinymce.init({
+        selector: "h1.editable",
+        inline: true,
+        toolbar: "undo redo",
+        menubar: false
+    });
+    tinymce.init({
+        selector:'textarea'
+    });
