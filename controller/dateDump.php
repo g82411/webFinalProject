964d2ed3fd519312133a92778d52f30ab76a2c24
= <lmz8241@nisra.net>
2014-06-02 18:06:08 +0800

    abc

--- controller/dateDump.php
+++ controller/dateDump.php
@@ -1,6 +1,6 @@
 <?php
 require 'connect.php';
-$date=mysql_query("SELECT BID,bookTitle,imageURL,date FROM Book ORDER BY date DESC LIMIT 20");
+$date=mysql_query("SELECT * FROM Book ORDER BY date DESC LIMIT 20");
 	while($dateDump=mysql_fetch_array($date)){
 	echo json_encode($dateDump);
 	}
--- js/index.js
+++ js/index.js
@@ -5,3 +5,23 @@ $("#login").click(function(){
 $("#register").click(function(){
   document.location.href="register.html";
 });
+$(function(){ 
+  $.ajax({
+    type: "GET",
+    url: "./controller/hotDump.php"
+  })
+  .done(function(data) {
+  	data=JSON.parse(data);
+     $('#hotPanel').html('<a href=\"./book.html#bid='+data.BID+'\">'+data.bookTitle+'</a>');
+});
+});
+$(function(){ 
+  $.ajax({
+    type: "GET",
+    url: "./controller/dateDump.php"
+  })
+  .done(function(data) {
+  	data=JSON.parse(data);
+     $('#datePanel').html('<a href=\"./book.html#bid='+data.BID+'\">'+data.bookTitle+'</a>');
+});
+});
