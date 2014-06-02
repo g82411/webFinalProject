c1d9d28c0b54ea1634cdf4355fb1ad10aca7d663
lagane100 <lagane100@lagane100deMacBook-Air.local>
2014-06-02 20:27:28 +0800

    delete comment.js
    merged with editComment.js

--- js/comment.js
+++
@@ -1,9 +0,0 @@
-tinymce.init({
-        selector: "h1.editable",
-        inline: true,
-        toolbar: "undo redo",
-        menubar: false
-    });
-    tinymce.init({
-        selector:'textarea',
-    });
