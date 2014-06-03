2195536ca1c5c2b026ad7a962709be942ec31ee1
lagane100 <lagane100@gmail.com>
2014-06-04 02:23:49 +0800

    change

--- js/register.js
+++ js/register.js
@@ -1,15 +1,12 @@
-$("#register").click(function(){
-  $.post("./controller/register.php",
+$("#registe").click(function(){
+  $.post("controller/register.php",
   {
     "username":$('input[id="username"]').val(),
     "password":$('input[id="password"]').val(),
     "nickname":$('input[id="nickname"]').val(),
     "language":$("#language").find(":selected").text(),
-	"profession":$("#profession").find(":selected").text(),
-    "email":$('input[id="email"]').val()
-    
-
-
+    "profession":$("#profession").find(":selected").text(),
+    "email":$('input[id="email"]').val(),
   },
   function(data){
 	alert(data);
